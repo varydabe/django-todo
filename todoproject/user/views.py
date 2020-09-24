ac9ab1114649357e9631668735ca4f9cbe0a42ab
varyd.abe <varyd.abe@gmail.com>
2020-09-24 20:55:41 +0700

    Add Create, Update, and Delete method API

--- todoproject/user/views.py
+++ todoproject/user/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.hashers import make_password
+from django.core.serializers import json
 from django.shortcuts import render
 from todoproject.response import Response
 from .models import Users
@@ -6,15 +8,56 @@ from . import transformer
 
 # Create your views here.
 def index(request):
-    user = Users.objects.all()
-    user = transformer.transform(user)
-    return Response.ok(values=user)
+    if request.method == 'GET':
+        user = Users.objects.all()
+        user = transformer.transform(user)
+        return Response.ok(values=user)
+
+    elif request.method == 'POST':
+        json_data = json.loads(request.body)
+
+        user = Users()
+        user.name = json_data['name']
+        user.email = json_data['email']
+        user.password = make_password(password=json_data['password'])
+        user.save()
+
+        return Response.ok(
+            values=transformer.single_transform(user),
+            message="Added!"
+        )
 
 def show(request, id):
-    user = Users.objects.filter(id=id).first()
+    if request.method == 'GET':
+        user = Users.objects.filter(id=id).first()
 
-    if not user:
-        return Response.bad_request(message='Pengguna tidak ditemukan!')
+        if not user:
+            return Response.bad_request(message='Pengguna tidak ditemukan!')
 
-    user = transformer.single_transform(user)
-    return Response.bad_request(values=user)
+        user = transformer.single_transform(user)
+        return Response.bad_request(values=user)
+    elif request.method == 'PUT':
+        json_data = json.loads(request.body)
+        user = Users.objects.filter(id=id).first()
+
+        if not user:
+            return Response.bad_request(message='Pengguna tidak ditemukan!')
+
+        user.name = json_data['name']
+        user.email = json_data['email']
+        user.password = make_password(password=json_data['password'])
+        user.save()
+
+        return Response.ok(
+            values=transformer.single_transform(user),
+            message="Updated!"
+        )
+    elif request.method == 'DELETE':
+        user = Users.objects.filter(id=id).first()
+        if not user:
+            return Response.bad_request(message='Pengguna tidak ditemukan')
+
+        user.delete()
+        return Response.ok(message='Deleted!')
+    else:
+        return Response.bad_request(message='Invalid Method!')
